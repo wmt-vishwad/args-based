55da28802e094e4f09517c88320183761afe2193
wmt-vishwad <86236386+wmt-vishwad@users.noreply.github.com>
2021-06-29 13:03:07 +0530

    Create sub.py

---
+++ sub.py
@@ -0,0 +1,16 @@
+
+import sys
+n = len(sys.argv)
+print("Total arguments passed:", n)
+
+print("\nName of Python:", sys.argv[0])
+
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+	print(sys.argv[i], end = " ")
+	
+Sub = 0
+for i in range(1, n):
+	Sub -= int(sys.argv[i])
+	
+print("\n\nResult:", Sub)
